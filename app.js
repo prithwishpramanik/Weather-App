4b9fe096885037b1187c3f593167912d59f84311
PRITHWISH PRAMANIK <prithwishpramanik462@gmail.com>
2021-04-25 10:57:55 +0530

    stable vaersion 1.0

--- app.js
+++ app.js
@@ -1,3 +1,45 @@
 let weather ={
-    "apikey":"6d5c4de09db8370fe0689c33513e5fdf"
-}
+    apikey:"6d5c4de09db8370fe0689c33513e5fdf",
+    fetchWeather: function(city){
+        fetch("https://api.openweathermap.org/data/2.5/weather?q="
+            + city
+            + "&units=metric&appid="
+            + this.apikey
+        )
+            .then((response) => response.json())
+            .then((data) => this.displayWeather(data));
+    },
+
+    displayWeather: function(data){
+        const { name } =data;
+        const { icon,description } = data.weather[0];
+        const{ temp,humidity } = data.main;
+        const{ speed } = data.wind;
+        console.log(name,icon,description,temp,humidity,speed);
+        document.querySelector(".city").innerText = "Weather in "+name;
+        document.querySelector(".icon").src="http://openweathermap.org/img/w/" + icon + ".png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp + " °C";
+        document.querySelector(".humidity").innerText = "Humidity:"+ humidity+"%";
+        document.querySelector(".wind").innerText ="Wind Speed: " + speed + " Kmph";
+        document.querySelector(".weather").classList.remove("loading");
+        document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?"+ name +"')";
+
+    },
+    search: function(){
+        this.fetchWeather(document.querySelector(".search-bar").value);
+    }
+};
+
+document.querySelector(".search button").addEventListener("click",function(){
+    weather.search();
+
+});
+
+document.querySelector(".search-bar").addEventListener("keyup",function(event){
+    if(event.key == "Enter"){
+        weather.search();
+    }
+    });
+
+weather.fetchWeather("Haldia");

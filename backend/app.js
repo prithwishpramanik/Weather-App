30b0b6bf07bd239554628afb7d47b005411641d1
Mitrajit Chandra Chandra <chandra.rupam@gmail.com>
2021-06-24 18:19:21 +0530

    Get location based on IP (#5)
    * Getting geo location using geoip-lite
    
    * Organised repository

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,7 @@ var cors = require("cors");
 const app = express();
 const port = process.env.PORT;
 const path = require('path');
+const geoip = require('geoip-lite');
 
 // see for info https://expressjs.com/en/guide/behind-proxies.html
 app.set('trust proxy', 1);
@@ -38,8 +39,13 @@ app.get("/api/search", async (req, res) => {
   try {
     if (!siteorigin)
     throw new Error("Unauthorised API call");
-    const searchString = `q=${req.query.q}`;
-    // It uses node-fetch to call the goodreads api, and reads the key from .env
+    var searchString = `${req.query.q}`;
+    if (searchString=="self"){
+      var ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress || null;
+      var data = geoip.lookup(ip);
+      searchString=data.city;
+    }
+    console.log(searchString);
     const response = await fetch(
       `https://api.openweathermap.org/data/2.5/weather?${searchString}&units=metric&appid=${process.env.WEATHER_API_KEY}`,
     );
